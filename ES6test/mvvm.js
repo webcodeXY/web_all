ff6723080c2053db29c83473422982f206a8afd7
xin <18255023533@163.com>
2020-11-23 16:27:10 +0800

    添加监听和input双向绑定
    Signed-off-by: xin <18255023533@163.com>

--- ES6test/mvvm.js
+++ ES6test/mvvm.js
@@ -1,8 +1,38 @@
 
-class Xvue {
+class Xvue extends EventTarget{
     constructor(options) {
+        super();
         this.$options = options;
         this.compile();
+        this.observe(this.$options.data);
+    }
+
+    observe(data){
+        let keys = Object.keys(data);
+        keys.forEach(key=>{
+            this.defineReact(data,key,data[key]);
+        })
+    }
+
+    defineReact(data,key,value){
+        let _this = this;
+
+        Object.defineProperty(data,key,{
+            configurable:true,
+            enumerable:true,
+            get() {
+                console.log("getValue")
+                return value;
+            },
+            set(newValue) {
+                console.log("setValue...",newValue);
+                let event = new CustomEvent(key,{
+                    detail:newValue
+                });
+                _this.dispatchEvent(event)
+                value = newValue;
+            }
+        })
     }
 
     compile(){
@@ -14,6 +44,20 @@ class Xvue {
         console.log(childNodes)
         childNodes.forEach(node=>{
             if(node.nodeType === 1){
+                let attr = node.attributes;
+                [...attr].forEach(attr=>{
+                    console.log(attr);
+                    let attrName = attr.name;
+                    let attrValue = attr.value;
+                    if(attrName.indexOf("v-") === 0){
+                        attrName = attrName.substr(2);
+                        node.value = this.$options.data[attrValue];
+                        node.addEventListener("input",evt => {
+                            this.$options.data[attrValue] = evt.target.value;
+                        })
+                    }
+                })
+
                 if(node.childNodes.length > 0){
                     this.compileNodes(node);
                 }
@@ -22,7 +66,15 @@ class Xvue {
                 let reg = /\{\{\s*(\S*)\s*\}\}/;
                 if(reg.test(content)){
                     let $1 = RegExp.$1;
-                    node.textContent = node.textContent.replace(reg,this.$options.data[$1])
+                    node.textContent = node.textContent.replace(reg,this.$options.data[$1]);
+                    this.addEventListener($1,evt => {
+                        console.log(evt.detail);
+                        console.log(this.$options.data[$1]);
+                        let oldValue = this.$options.data[$1];
+                        let reg = new RegExp(oldValue);
+                        node.textContent = node.textContent.replace(reg,evt.detail);
+                        //node.textContent = node.textContent.replace(reg,this.$options.data[$1]);
+                    });
                 }
             }
         })
